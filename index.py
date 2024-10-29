2c1bf08722261fe206f35294451429ba0a092227
Mrbailish <135006238+Mrbailish@users.noreply.github.com>
2024-10-29 20:08:43 +0800

    Update index.py

--- index.py
+++ index.py
@@ -4,12 +4,12 @@ col,col1, = st.columns(2)
 
 with col:
     st.image("https://imgs.699pic.com/images/601/338/156.jpg!list2x.v1",use_column_width=True)
-    flag = st.button("绘言",use_container_width=True)
+    flag = st.button("绘声绘色",use_container_width=True)
     if flag:
         st.switch_page("pages/demo-history.py")
 #
 with col1:
     st.image("https://imgs.699pic.com/images/400/913/760.jpg!list2x.v1", use_column_width=True)
-    flag = st.button("绘图",use_container_width=True)
+    flag = st.button("绘图绘画",use_container_width=True)
     if flag:
         st.switch_page("pages/textToImage.py")
