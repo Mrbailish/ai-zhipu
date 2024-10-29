7f25282e806db1376d8458c6e42ed55146d6c233
Mrbailish <135006238+Mrbailish@users.noreply.github.com>
2024-10-29 19:26:15 +0800

    Update demo.py

--- pages/demo.py
+++ pages/demo.py
@@ -1,16 +1,16 @@
 '''
 streamlit多页面程序的入口文件
 '''
-from tkinter import Image
+# from tkinter import Image
 
-import streamlit as st
-st.title("AI大模型应用产品网")
-col,col1,col2 = st.columns(2)
+# import streamlit as st
+# st.title("AI大模型应用产品网")
+# col,col1,col2 = st.columns(2)
 
-with col:
-    st.image(Image.open('img1.jpg'))
-    st.button("jory慧言")
-    st.switch_page("pages/demo.py")
+# with col:
+#     st.image(Image.open('img1.jpg'))
+#     st.button("jory慧言")
+#     st.switch_page("pages/demo.py")
 
 
 # c1,c2,c3 = st.columns(3)
