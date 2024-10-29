406fc2cca96c671d07163a0c9134bf0f9b0f5032
Mrbailish <135006238+Mrbailish@users.noreply.github.com>
2024-10-29 20:07:08 +0800

    Update index.py

--- index.py
+++ index.py
@@ -3,13 +3,13 @@ st.title("🙋🙋 AI大模型应用产品网 🙋🙋")
 col,col1, = st.columns(2)
 
 with col:
-    st.image("https://th.bing.com/th/id/OIP.0NipysZAT58B_EvnMQsOewHaId?pid=ImgDet&w=178&h=203&c=7",use_column_width=True)
+    st.image("https://imgs.699pic.com/images/601/338/156.jpg!list2x.v1",use_column_width=True)
     flag = st.button("绘言",use_container_width=True)
     if flag:
         st.switch_page("pages/demo-history.py")
 #
 with col1:
-    st.image("https://th.bing.com/th/id/OIP.0NipysZAT58B_EvnMQsOewHaId?pid=ImgDet&w=178&h=203&c=7", use_column_width=True)
+    st.image("https://imgs.699pic.com/images/400/913/760.jpg!list2x.v1", use_column_width=True)
     flag = st.button("绘图",use_container_width=True)
     if flag:
         st.switch_page("pages/textToImage.py")
