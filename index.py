21a915ce74e229dad7cd9922a0d10788e2e1fd97
Mrbailish <135006238+Mrbailish@users.noreply.github.com>
2024-10-30 22:25:41 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,15 +1,20 @@
 import streamlit as st
 st.title("🙋🙋 AI大模型应用产品网 🙋🙋")
-col,col1, = st.columns(2)
+col,col1,col2 = st.columns(3)
 
 with col:
-    st.image("https://imgs.699pic.com/images/601/338/156.jpg!list2x.v1",use_column_width=True)
-    flag = st.button("绘声绘色",use_container_width=True)
+    st.image("https://t7.baidu.com/it/u=2656780661,2835874604&fm=193&f=GIF",use_column_width=True)
+    flag = st.button("绘言",use_container_width=True)
     if flag:
         st.switch_page("pages/demo-history.py")
 #
 with col1:
-    st.image("https://imgs.699pic.com/images/400/913/760.jpg!list2x.v1", use_column_width=True)
-    flag = st.button("绘图绘画",use_container_width=True)
+    st.image("https://t7.baidu.com/it/u=2656780661,2835874604&fm=193&f=GIF", use_column_width=True)
+    flag = st.button("绘图",use_container_width=True)
     if flag:
         st.switch_page("pages/textToImage.py")
+with col2:
+    st.image("https://t7.baidu.com/it/u=2656780661,2835874604&fm=193&f=GIF", use_column_width=True)
+    flag = st.button("岗位",use_container_width=True)
+    if flag:
+        st.switch_page("pages/job-ai.py")
